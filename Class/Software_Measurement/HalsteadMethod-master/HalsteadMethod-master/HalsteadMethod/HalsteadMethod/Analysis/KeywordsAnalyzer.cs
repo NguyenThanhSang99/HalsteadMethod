d08d9336b40c8c82bdd3dd2f247f735ed9069a40
Thuyên Phạm <phamvanthuyen2802@gmail.com>
2020-06-06 20:43:36 +0700

    Total of Operator

---
+++ Class/Software_Measurement/HalsteadMethod-master/HalsteadMethod-master/HalsteadMethod/HalsteadMethod/Analysis/KeywordsAnalyzer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalsteadMethod.Analysis
+{
+	class KeywordsAnalyzer
+	{
+		public static  KeywordsAnalyzer _instance;
+		private static String[] keywords = { "auto", "break", "case", "char", "const", "continue", "default", "do",
+			"double", "else", "enum", "extern", "float", "for", "goto", "if", "int", "long", "register", "return",
+			"short", "signed", "sizeof", "static", "struct", "switch", "typedef", "union", "unsigned", "void",
+			"volatile", "while" };
+
+
+		private KeywordsAnalyzer()
+		{
+
+		}
+
+		public static KeywordsAnalyzer Instance {
+			get { if (_instance == null) _instance = new KeywordsAnalyzer();
+				return _instance; }
+			set => _instance = value;
+		}
+
+		public static int size()
+		{
+			return keywords.Length;
+		}
+
+		public bool isKeyword(String keyword)
+		{
+			bool result = false;
+			for (int i = 0; i < size(); ++i)
+			{
+				if (keywords[i].Equals(keyword))
+				{
+					result = true;
+					break;
+				}
+			}
+			return result;
+		}
+	}
+}
---
+++ Class/Software_Measurement/HalsteadMethod-master/HalsteadMethod-master/HalsteadMethod/HalsteadMethod/Main_Form.cs
@@ -0,0 +1,103 @@
+﻿using HalsteadMethod.Analysis;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace HalsteadMethod
+{
+    public partial class Main_Form : Form
+    {
+        //Array to save path of cs files
+        private String[] files;
+        public Main_Form()
+        {
+            files = null;
+            InitializeComponent();
+        }
+        /// <summary>
+        /// When user click choose files, system open dialog to choose files
+        /// </summary>
+        private void button_choose_files_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog chooseFileDialog = new OpenFileDialog();
+                chooseFileDialog.Filter = "cs files (*.cs)|*.cs|All files (*.*)|*.*";
+                chooseFileDialog.FilterIndex = 1;
+                chooseFileDialog.Multiselect = true;
+
+                if (chooseFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string sFileName = chooseFileDialog.FileName;
+                    string[] arrAllFiles = chooseFileDialog.FileNames; //used when Multiselect = true  
+                    this.files = arrAllFiles;
+                }
+            }
+            catch (Exception err)
+            {
+                //If error show it in output
+                Debug.Print(err.ToString());
+            }
+        }
+        /// <summary>
+        /// When user click choose folder, system open dialog to choose folder
+        /// </summary>
+        private void button_select_folder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowerDialog = new FolderBrowserDialog();
+            folderBrowerDialog.Description = "Custom Description";
+
+            if (folderBrowerDialog.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox_path.Text = folderBrowerDialog.SelectedPath;
+            }
+            String folder = this.textBox_path.Text;
+            try
+            {
+                this.files = Directory.GetFiles(folder, "*.cs", SearchOption.AllDirectories);
+            }
+            catch (Exception err)
+            {
+                //If error show it in output
+                Debug.Print(err.ToString());
+            }
+        }
+
+        private void button_analyse_Click(object sender, EventArgs e)
+        {
+            int totalKeywords = 0;
+            int totalSymbols = 0;
+            try
+            {
+                String allFiles = "";
+                String text = "";
+                foreach (String file in files)
+                {
+                   allFiles = allFiles + " " + file + "\r\n";
+                    text = File.ReadAllText(file);
+                }
+
+
+                //this.textBox_result.Text = text;
+                String[] words = text.Split(' ');
+
+                for(int i = 0; i < words.Length; i++)
+                {
+                    if (KeywordsAnalyzer.Instance.isKeyword(words[i])){
+                        totalKeywords++;
+                    }
+                    if (SymbolAnalyzer.Instance.isSymbols(words[i]))
+                    {
+                        totalSymbols++;
+                    }
+                }
+                this.textBox_result.Text = "Tổng số toán tử :" + (totalKeywords + totalSymbols);
+            }
+            catch (Exception err)
+            {
+                Debug.Print(err.ToString());
+            }
+        }
+    }
+}
